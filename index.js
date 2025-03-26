6ec3db15a8fffc1105bb459e5f0c7f0a88c46c06
eduardolm1 <eduardolm300601@gmail.com>
2025-03-26 19:53:38 +0100

    Terminando ejercicios

---
+++ index.js
@@ -0,0 +1,125 @@
+//1
+const Coche ={
+    marca:'',
+    modelo:'',
+    matricula:''
+}
+//2
+const Casa ={
+    codPostal:'',
+    calle:'',
+    portal:'',
+    piso:''
+}
+//3
+
+const FullStackDeveloper={
+    lenguajes:[],
+    proyectos:[],
+}
+//4
+
+const Perro ={
+    nombre:'',
+    raza:'',
+    color:'',
+    edad:'',
+    ladrar: function(){
+        return console.log('ladrar')
+    },
+    popo: function(){
+        return ''
+    }
+
+}
+//5
+
+marcaPortatil = Portatil.marca
+//6
+marcaPortatil2  = Portatil.marca
+
+//7
+grupos = Concierto.grupos
+//8
+RGB = [
+    rojo=Led.rojo,
+    azul=Led.azul,
+    verde=Led.verde
+]
+
+//9
+Portatil.modelo = 'P345'
+
+//10
+Concierto.cartelera.push("Guns N' Roses") 
+//11
+Concierto.fecha = new Date()
+
+//12
+Impresora.imprimiendo = {
+    nombreArchivo:'',
+    copias:'',
+    numPaginas:''
+}
+
+//13
+const Noticia ={
+    titular:'',
+    cuerpo:''
+}
+//14
+const Persona= {
+    nombre:'',
+    apellidos:'',
+    edad:''
+}
+//15
+const Avion ={
+    numPasajeros:'',
+    despegar: function(){
+        return console.log('despegando')
+    },
+    volar:function(){
+        return console.log('llegando al destino')
+    },
+    aterrizar:function(){
+        return console.log('aterrizando')
+    }
+}
+
+//16 
+const Paquete={
+    contenido:[]
+}
+
+//17
+const Pais={
+    numHabitantes:'',
+    continente:'',
+    gentilicio:''
+}
+
+//18
+const codError = O_Error.codigo
+
+//19
+const integrantes =Grupo.integrantes
+//20
+
+const nivelesTinta=Impresora.tinta
+//21
+
+const pixeles= Pantalla.pixeles
+//22
+especificaciones =Movil['especificaciones']
+
+//23
+Grupo.numIntegrantes= 5
+//24
+Pantalla.dimensiones= '1920x1080'
+
+//25
+Led.encendido = !Led.encendido
+
+//26
+Movil.temperatura = '20º'
